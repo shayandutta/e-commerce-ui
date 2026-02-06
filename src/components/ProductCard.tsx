45842a7935641721be0975a35017ac9e1090ece1
shayandutta <shayandutta23@gmail.com>
2026-02-06 23:46:27 +0530

    feat: product card updated

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,12 +3,100 @@
 import Image from "next/image";
 
 import { ProductType } from "@/types";
+import Link from "next/link";
+import { ShoppingCartIcon } from "lucide-react";
+import { useState } from "react";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
   //props with the type of the product -> basically saying that we are going to get product from the parent component as a prop and the type of the product is ProductType. So types need to be mentioned here and also in the parent component.
+  const [productTypes, setProductTypes] = useState({
+    //initial state of the product types
+    size: product.sizes[0],
+    color: product.colors[0],
+  });
+
+  const handleProductTypeChange = ({
+    type,
+    value,
+  }: {
+    type: "size" | "color";
+    value: string;
+  }) => {
+    setProductTypes((prev) => ({
+      ...prev,
+      [type]: value,
+    }));
+  };
+
   return (
-    <div className="w-full">
-        Product Card
+    <div className="shadow-lg rounded-lg overflow-hidden ">
+      {/* IMAGE */}
+      <Link href={`/products/${product.id}`}>
+        <div className="relative aspect-[2/3]">
+          <Image
+            src={product.images[productTypes.color]}
+            alt={product.name}
+            fill
+            className="object-cover hover:scale-105 transition-all duration-300"
+          />
+        </div>
+      </Link>
+      {/* PRODUCT DETAILS */}
+      <div className="flex flex-col gap-4 p-4">
+        {/* PRODUCT NAME */}
+        <h1 className="text-md font-medium">{product.name}</h1>
+        {/* PRODUCT DESCRIPTION */}
+        <p className="text-sm text-gray-500">{product.shortDescription}</p>
+        {/* PRODUCT TYPES */}
+        <div className="flex items-center gap-4 text-xs">
+          {/* SIZES */}
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-500">Sizes</span>
+            <select
+              name="size"
+              id="size"
+              className="ring ring-gray-300 rounded-md px-2 py-1"
+              onChange={(e) =>
+                handleProductTypeChange({ type: "size", value: e.target.value })
+              }
+            >
+              {product.sizes.map((size) => (
+                <option key={size} value={size}>
+                  {size.toUpperCase()}
+                </option>
+              ))}
+            </select>
+          </div>
+          {/* COLORS */}
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-500">Colors</span>
+            <div className="flex items-center gap-1">
+              {product.colors.map((color) => (
+                <div
+                  className={`cursor-pointer border-1 ${productTypes.color === color ? "border-gray-400" : "border-gray-200"} rounded-full p-[1.2px]`}
+                  key={color}
+                  onClick={() =>
+                    handleProductTypeChange({ type: "color", value: color })
+                  }
+                >
+                  <div
+                    className="w-[14px] h-[14px] rounded-full"
+                    style={{ backgroundColor: color }}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        {/* PRICE & ADD TO CART */}
+        <div className="flex items-center justify-between">
+          <p className="font-medium">${product.price.toFixed(2)}</p>
+          <button className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm hover:text-white hover:bg-black transition-all duration-300 flex items-center gap-2">
+            <ShoppingCartIcon className="w-4 h-4" />
+            Add to Cart
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
